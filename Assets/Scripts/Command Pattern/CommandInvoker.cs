f641625c0966f2608446f631f9213d29b4d50fa6
Joe-CDN <35740892+Joe-CDN@users.noreply.github.com>
2021-10-22 16:04:02 -0400

    With command pattern

---
+++ Assets/Scripts/Command Pattern/CommandInvoker.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CommandInvoker : MonoBehaviour
+{
+    static Queue<ICommand> commandBuffer;
+
+    static List<ICommand> commandHistory;
+    static int counter;
+
+    private void Awake() 
+    {
+        commandBuffer = new Queue<ICommand>();
+        commandHistory = new List<ICommand>();
+    }
+
+    public static void AddCopmmand(ICommand command)
+    {
+        while(commandHistory.Count > counter)
+        {
+            commandHistory.RemoveAt(counter);
+        }
+        
+        commandBuffer.Enqueue(command);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (commandBuffer.Count > 0)
+        {
+            ICommand c = commandBuffer.Dequeue();
+            c.Execute();
+            
+            //commandBuffer.Dequeue().Execute();
+
+            commandHistory.Add(c);
+            counter++;
+            Debug.Log("Command history length: " + commandHistory.Count);
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.Z))
+            {
+                if (counter > 0)
+                {
+                    counter--;
+                    commandHistory[counter].Undo();
+                }
+            }
+            else if (Input.GetKeyDown(KeyCode.R))
+            {
+                if (counter < commandHistory.Count)
+                {
+                    commandHistory[counter].Execute();
+                    counter++;
+                }
+            }
+        }
+    }
+}
